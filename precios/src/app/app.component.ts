5bb315ea5d38e25ff95bfd9a5de21f47930bbedd
Astar18 <62781005+Astar18@users.noreply.github.com>
2024-02-20 21:29:05 -0600

    cambio

--- precios/src/app/app.component.ts
+++ precios/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import { ApiService } from './service/api.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,20 +9,13 @@ export class AppComponent implements OnInit{
   title = 'precios';
   datos: any;
 
-  constructor(private apiService: ApiService) { }
+  
 
   ngOnInit(): void {
     this.obtenerDatos();
   }
 
   obtenerDatos(): void {
-    this.apiService.obtenerDatos().subscribe(
-      (data) => {
-        this.datos = data;
-      },
-      (error) => {
-        console.error('Error al obtener datos:', error);
-      }
-    );
+   
   }
 }
